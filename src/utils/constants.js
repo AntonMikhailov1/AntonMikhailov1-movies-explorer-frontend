3c7bbe9c2689c9169e3c6fe21cbe8637b518e3bf
AntonMikhailov1 <mikhailovanton.nsk@gmail.com>
2023-11-22 01:22:52 +0300

    fix: change api url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 const MOVIES_API_URL = 'https://api.nomoreparties.co';
-const MAIN_API_URL = 'http://localhost:3000';
+const MAIN_API_URL = 'https://api.movie-search.nomoredomainsrocks.ru';
 
  export { MOVIES_API_URL, MAIN_API_URL };
